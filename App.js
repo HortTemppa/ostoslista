6dfd079f9e3ecc21ade5b78d0334f9d5a85a6b63
Teemu Laine <teemu.laine@myy.haaga-helia.fi>
2022-08-25 19:35:24 +0300

    creating basic structure for the app

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
+  const [list, setList] = useState([]);
+  const [newItem, setNewItem] = useState();
+
+  const handleItemChange = (item) => {
+    setNewItem(item);
+  };
+
+  const handleAdd = () => {
+    setList([...list, { item: { item }, key: list.length + 1 }]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput onChange={handleItemChange} value={newItem} />
+      <Button onPress={handleAdd} title="Add Item" />
+      <Button onPress={handleClear} title="Clear shopping list" />
     </View>
   );
 }
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
